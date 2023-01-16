e49a9d9e0291f4de048df00332f6b0b89d9ddacf
Crowdbotics <team@crowdbotics.com>
2023-01-16 21:33:23 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/resolution_38321/MainActivity.java
@@ -0,0 +1,15 @@
+package com.resolution_38321;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "resolution_38321";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "resolution-38321",
+  url: "https://resolution-38321.botics.co",
+  api: "https://resolution-38321.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
